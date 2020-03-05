4bcd9f79e2ea71aaed3f8c85f365f4d0a3ac500b
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-03-05 08:23:48 +0100

    chore: fix types

--- src/plugin.ts
+++ src/plugin.ts
@@ -89,7 +89,7 @@ export const buildHandler = (
 
   return async (req, res): Promise<void> => {
     if (!admin) {
-      let beforeResult = null;
+      let beforeResult: AdminBroOptions | null | undefined = null;
       if (options.before) {
         beforeResult = await options.before();
       }
