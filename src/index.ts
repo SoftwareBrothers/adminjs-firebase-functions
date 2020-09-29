72795ebc8ed11d995a1e2f8693652089c6ee241f
Wojciech <wojciech.g.krysiak@gmail.com>
2020-09-29 06:20:11 +0200

    fix: export all types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-export * from './utils/parse-files';
 export * from './plugin';
-export * from './utils/routes';
+export * from './utils';
---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export * from './compute-root-paths';
+export * from './parse-files';
+export * from './routes';
+export * from './prepare-compare-path';
